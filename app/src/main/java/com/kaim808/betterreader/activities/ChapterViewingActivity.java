e5dad261461e339696b6a3fce6d30640384cadcb
Kai Munechika <kaim808@gmail.com>
2017-07-24 03:19:38 -1000

    added action bar

--- app/src/main/java/com/kaim808/betterreader/activities/ChapterViewingActivity.java
+++ app/src/main/java/com/kaim808/betterreader/activities/ChapterViewingActivity.java
@@ -6,8 +6,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.kaim808.betterreader.MangaEdenApiInterface;
@@ -27,11 +30,24 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+// TODO: 7/23/17 add space between pages in our ViewPager
+// TODO: 7/24/17 add app bar
+// TODO: 7/24/17 add bottom navigation ui
+
+
 public class ChapterViewingActivity extends AppCompatActivity {
 
-    @BindView(R.id.view_pager) ViewPagerFixed mPager;
+    @BindView(R.id.view_pager)
+    ViewPagerFixed mPager;
+    @BindView(R.id.chapter_viewing_toolbar)
+    Toolbar mToolbar;
+
     private PagerAdapter mPagerAdapter;
     private String[] mImageUrls;
+
+    private static String testTitle = "Boku no Hero Academia 117";
+    private static String testSubtitle = "C’mon, Rappa, Let’s Have Ourselves a Match!!";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +55,44 @@ public class ChapterViewingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chapter_viewing);
         ButterKnife.bind(this);
 
+        initializeActionBar();
+
         String chapterId = getIntent().getStringExtra(HomeActivity.SELECTED_CHAPTER);
-        Log.e("kaikai", chapterId);
 
         chapterCall(RetrofitSingleton.mangaEdenApiInterface, chapterId);
 
-
     }
 
-    // this belongs in chapter viewing activity
+    private void initializeActionBar() {
+        mToolbar.setTitle(testTitle);
+        mToolbar.setSubtitle(testSubtitle);
+        setSupportActionBar(mToolbar);
+
+        // if I decide to use the up action, gotta declare the parent activity in the manifest
+//        ActionBar ab = getSupportActionBar();
+//        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chapter_viewing_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_lorem:
+                Toast.makeText(this, "lorem", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_ipsum:
+                Toast.makeText(this, "ipsum", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void chapterCall(MangaEdenApiInterface apiInterface, String chapterId) {
         Call<Chapter> chapterPagesCall = apiInterface.getChapter(chapterId);
         chapterPagesCall.enqueue(new Callback<Chapter>() {
@@ -68,13 +113,11 @@ public class ChapterViewingActivity extends AppCompatActivity {
         });
     }
 
-
-
     private String[] getImageUrls(Chapter chapter) {
         List<List<String>> images = chapter.getImages();
         String[] imageUrls = new String[images.size()];
 
-        for(int i = 0; i < images.size(); i++) {
+        for (int i = 0; i < images.size(); i++) {
             imageUrls[i] = (getImageUrlSuffix(images, i));
         }
 
@@ -101,18 +144,26 @@ public class ChapterViewingActivity extends AppCompatActivity {
         if (event.getAction() == KeyEvent.ACTION_DOWN) {
             switch (keyCode) {
                 case KeyEvent.KEYCODE_VOLUME_UP:
-                    if (currentPageNum < mImageUrls.length - 1) {
-                        mPager.setCurrentItem(currentPageNum + 1);
-                    }
+                    gotoNextPage(currentPageNum);
                     return true;
                 case KeyEvent.KEYCODE_VOLUME_DOWN:
-                    if (currentPageNum > 0) {
-                        mPager.setCurrentItem(currentPageNum - 1);
-                    }
+                    gotoPreviousPage(currentPageNum);
                     return true;
             }
         }
         return super.dispatchKeyEvent(event);
+    }
+
+    private void gotoNextPage(int currentPageNum) {
+        if (currentPageNum < mImageUrls.length - 1) {
+            mPager.setCurrentItem(currentPageNum + 1);
+        }
+    }
+
+    private void gotoPreviousPage(int currentPageNum) {
+        if (currentPageNum > 0) {
+            mPager.setCurrentItem(currentPageNum - 1);
+        }
     }
 
     private class ChapterPageAdapter extends FragmentStatePagerAdapter {
@@ -131,6 +182,9 @@ public class ChapterViewingActivity extends AppCompatActivity {
             return mImageUrls.length;
         }
     }
+
+
+
 }
 
 
